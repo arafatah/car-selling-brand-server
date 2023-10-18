22a5cc5484fab8d7396fbc60545a13efe5144018
arafatah <arafathowlader456@gmail.com>
2023-10-19 04:48:10 +0600

    app.get added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,10 +11,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});     
-
-// automobile
-// 9eFMaQCqdl9sV4Oe
+});
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.dokkyfc.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,32 +22,41 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const carCollection = client.db("automobile").collection("cars");
+
+    app.get("/cars", async (req, res) => {
+      const cursor = carCollection.find({});
+      const cars = await cursor.toArray();
+      res.json(cars);
+    });
+
+    app.post("/addCar", async (req, res) => {
+      const newCar = req.body;
+      const result = await carCollection.insertOne(newCar);
+      console.log(result);
+      res.json(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
-
-
-
-
-
